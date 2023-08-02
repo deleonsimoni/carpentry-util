7c034ffbbb3f9b50c3e9bf8b6e5137e8237cb2d0
Deleon Simoni <deleon.simoni>
2023-08-02 19:39:38 -0300

    v1 release

--- server/index.js
+++ server/index.js
@@ -2,12 +2,26 @@
 const config = require('./config/config');
 const app = require('./config/express');
 require('./config/mongoose');
+const https = require('https');
+const fs = require('fs');
+
+const https_options = {
+  ca: fs.readFileSync("/home/ubuntu/carpentryutil/certificado/ca_bundle.crt"),
+  key: fs.readFileSync("/home/ubuntu/carpentryutil/certificado/private.key"),
+  cert: fs.readFileSync("/home/ubuntu/carpentryutil/certificado/certificate.crt")
+};
+
+const httpsServer = https.createServer(https_options, app);
 
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`);
+  });
+
+  httpsServer.listen(8443, () => {
+    console.log('HTTPS Server running on port 443');
   });
 }
 
